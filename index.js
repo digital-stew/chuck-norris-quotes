636889e3c40a4e278e85ab4dad4dc68af9eaf312
Stewart Ridings <stewart@tux-systems.co.uk>
2023-01-05 22:47:34 +0000

    finished

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('yo sup')
+const readline = require('readline');
+const quotes = require('./quotes.json')
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+(async function run() {
+    const randomNumber = Math.floor(Math.random() * quotes.length + 1);
+    console.log(`\nSelected random number: ${randomNumber}\nYour random Chuck Norris quote is:\n${quotes[randomNumber]}`)
+    const userInput = await new Promise(resolve => rl.question('another? y/n ', resolve))
+    if (userInput.toLowerCase() !== 'y') process.exit()
+    run()
+})()
+
